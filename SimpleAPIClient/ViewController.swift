a42d13d373de310d4a010c09b7d96aa2beba930a
Kouki Saito <dan.addr.skd@gmail.com>
2014-10-19 15:01:44 +0900

    Initial Commit

---
+++ SimpleAPIClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleAPIClient
+//
+//  Created by Kouki Saito on 2014/10/19.
+//  Copyright (c) 2014年 Kouki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
